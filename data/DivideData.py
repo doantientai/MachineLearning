e3eb3be2a56366e0d17825243cdb48975e0650bd
Tai Christian <doantientai@gmail.com>
2017-09-25 11:37:39 +0700

    main

--- data/DivideData.py
+++ data/DivideData.py
@@ -1,9 +1,13 @@
 import os
+from shutil import copyfile
 
 PATH_TRAIN = '/home/tai-databases/CTLymphNodes/PNG/resize_256_gaus50paired/train/'
 PATH_VAL = '/home/tai-databases/CTLymphNodes/PNG/resize_256_gaus50paired/test/'
 PATH_TEST = '/home/tai-databases/CTLymphNodes/PNG/resize_256_gaus50paired/val/'
 PATH_SOURCE = '/home/tai-databases/CTLymphNodes/PNG/resize_256'
+PATH_DEST = '/GANoiseMaker/rival/DnCNN-crisb-DUT/DnCNN-tensorflow/data/CTLymphNodes_256'
+PATH_DEST_TRAIN = PATH_DEST + '_train'
+PATH_DEST_TEST = PATH_DEST + '_test'
 
 list_train = [os.path.join(PATH_TRAIN,fn) for fn in next(os.walk(PATH_TRAIN))[2]]
 list_test = [os.path.join(PATH_TEST,fn) for fn in next(os.walk(PATH_TEST))[2]]
@@ -14,3 +18,13 @@ print("list_train", len(list_train))
 print("list_test", len(list_test))
 print("list_val", len(list_val))
 print("list_source", len(list_source))
+
+def CopyImages(list_name, path_source, path_dest):
+	for a_name in list_name:
+		copyfile(path_source + a_name, path_dest + a_name)
+
+def main:
+	print("list_train", len(list_train))
+	print("list_test", len(list_test))
+	print("list_val", len(list_val))
+	print("list_source", len(list_source))
